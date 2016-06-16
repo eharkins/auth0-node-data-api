ca42fdedd59419afbc08c77bab767f7ca33d7aae
eli <eli.harkins@gmail.com>
2016-06-16 15:08:33 -0700

    submitting data

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ app.get('/secured/getFavGenre', function(req, res) {
 app.get('/secured/addSong', function(req, res) {
   //res.status(200).send("All good. You only get this message if you're authenticated");
   var song = req.song;
-  // console.log(song);
+  console.log(song);
   res.writeHead(200);
   res.end(song);
   //addSong(req.user.sub, song, res);
