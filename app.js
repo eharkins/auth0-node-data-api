7a9915165a0e55ee606c3c8a0d4356465845aa19
eli <eli.harkins@gmail.com>
2016-06-23 16:23:55 -0700

    rules

--- app.js
+++ app.js
@@ -232,8 +232,40 @@ function addSong(user_id, song, res){
     });
   });
 
+function getPlays(user_id, res){
+ 
+ console.log("getPlays method executing");
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting data...');
+
+  client
+    .query('SELECT total_plays as value FROM playlists WHERE user_id = $1', [user_id], function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      var plays = result.rows[0].value;
+
+      console.log(plays);
+      
+
+      
+      res.writeHead(200, {"Accept": "text/html"});
+      res.end(plays);
+      //console.log(result);
+    });
+  });
+
 
 };
+
+
+};
+
+app.get('/secured/getPlays', function(req, res){
+  console.log("getPlays");
+  getPlays(req.user.sub, res);
+});
 
 app.get('/secured/getSongs', function(req, res){
   console.log("getSongs");
