133fccce10e8617ccf71917d8c15fd55483554e0
eli <eli.harkins@gmail.com>
2016-08-01 11:54:17 -0700

    cleanup

---
+++ routes/genres.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('pg');
+db.defaults.ssl = true;
+
+
+function queryGenre(user_id, res){
+	
+ console.log("logging works");
+  db.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting data...');
+
+  client
+    .query('SELECT fav_genre as value FROM user_data WHERE user_id = $1', [user_id], function(err, result) {
+      console.log(result.rows[0].value);
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.writeHead(200, {"Accept": "text/html"});
+      res.end(result.rows[0].value);
+    });
+  });
+
+};
+
+
+router.get('/', function(req, res) {
+   queryGenre(req.user.sub, res);
+});
+
+// router.get('/', function(req, res, ) {
+//   res.send('respond with a resource');
+// });
+
+module.exports = router;
